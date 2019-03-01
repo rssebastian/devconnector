ea8a322f9eb8cae758cad88a08acdf61402c5ecb
Lol Cinnamonbuns <sebastiansryan@gmail.com>
2019-02-28 20:27:55 -0800

    added profile model, routes, and validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -65,7 +65,7 @@ router.post('/register', (req, res) => {
     })
 });
 
-// @route  GET api/users/register
+// @route  GET api/users/login
 // @desc   Login User / Returning JWT Token
 // @access Public
 
