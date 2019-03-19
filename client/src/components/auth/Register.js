941d2d9939230693d3cb02e8dbdf46de7159ab6d
unknown <sebastiansryan@gmail.com>
2019-03-19 14:37:02 -0700

    implemented Login and Register functionality with Redux

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import propTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import classnames from 'classnames';
 import { connect } from 'react-redux';
-import { registeruser } from '../../actions/authActions';
+import { registerUser } from '../../actions/authActions';
 
 class Register extends Component {
   constructor () {
@@ -15,6 +15,12 @@ class Register extends Component {
       password2: '',
       errors: {}
     };
+  }
+
+  componentDidMount () {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push('/dashboard');
+    }
   }
 
   componentWillReceiveProps (nextProps) {
@@ -36,7 +42,8 @@ class Register extends Component {
       password2: this.state.password2
     };
     
-    this.props.registeruser(newUser, this.props.history);
+    // Triggers registerUser action and sends new user data with router history
+    this.props.registerUser(newUser, this.props.history);
   }
   
   render() {
@@ -109,15 +116,20 @@ class Register extends Component {
   }
 }
 
+// Validates incoming props
 Register.propTypes = {
-  registerUser: propTypes.func.isRequired,
-  auth: propTypes.object.isRequired,
-  errors: propTypes.object.isRequired
+  registerUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
 };
 
+// Takes incoming state from store and maps it to props within component
 const mapStateToProps = (state) => ({
   auth: state.auth,
   errors: state.errors
 })
 
-export default connect(mapStateToProps, { registeruser })(withRouter(Register));
+// Connects the component to the store, maps store state to components props,
+// maps dispatch actions to the props, and executes on the component wrapped with
+// Router functionality
+export default connect(mapStateToProps, { registerUser })(withRouter(Register));
--- client/src/store.js
+++ client/src/store.js
@@ -4,9 +4,13 @@ import rootReducer from './reducers/'
 
 const initialState = {};
 
+// Thunk is a middleware that looks at every action that passes through the system, 
+// and if it’s a function, it calls that function
 const middleware = [thunk];
 
 // First parameter = root reducer
+// Second parameter = initial state
+// Third parameter = compose all middleware(runs right function to left)
 const store = createStore(
     rootReducer, 
     initialState,
